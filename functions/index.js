0a35b3cb60858c91be1df6a13da44bf1987d7ba7
Gon <gfarianunes@gmail.com>
2019-08-19 17:35:36 +0100

    mailgun integration and firebase functions3

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
 const functions = require('firebase-functions');
 const mailgun = require("mailgun-js");
-const DOMAIN = 'xxx';
-const api_key = 'key-xxx'
+const DOMAIN = '';
+const api_key = ''
 const mg = mailgun({apiKey: api_key, domain: DOMAIN});
 const data = {
-	from: 'Excited User <xxx@xxx.org>',
-	to: 'xxx@xxx.com',
+	from: '',
+	to: '',
 	subject: 'Hello',
 	text: 'Testing some Mailgun awesomness!'
 };
