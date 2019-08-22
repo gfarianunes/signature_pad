a0eb062468787ce272c116fea2310ab35ec0ae54
Gon <gfarianunes@gmail.com>
2019-08-22 16:47:45 +0100

    firebase auth working v1

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,5 @@
 const functions = require('firebase-functions');
-const mailgun = require("mailgun-js");
-const DOMAIN = '';
-const api_key = ''
-const mg = mailgun({apiKey: api_key, domain: DOMAIN});
-const data = {
-	from: '',
-	to: '',
-	subject: 'Hello',
-	text: 'Testing some Mailgun awesomness!'
-};
-mg.messages().send(data, function (error, body) {
-	console.log(body);
-});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
